1171e4a5a0dc21b99573cbee849bd614238b26c9
Michele Fasciano <michelefasciano37@hotmail.co.uk>
2022-08-23 19:07:25 +0100

    Delete functionality added

--- src/components/SingleTodo.tsx
+++ src/components/SingleTodo.tsx
@@ -18,6 +18,10 @@ const SingleTodo = ({ todo, todos, setTodos }: Props) => {
     ))
   }
 
+  const handleDelete = (id: number) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  }
+
   return (
     <form key={todo.id} className="todos__single">
       {todo.isDone ? (
@@ -29,7 +33,7 @@ const SingleTodo = ({ todo, todos, setTodos }: Props) => {
         <span className="icon">
           <AiFillEdit />
         </span>
-        <span className="icon">
+        <span className="icon" onClick={() => handleDelete(todo.id)}>
           <AiFillDelete />
         </span>
         <span className="icon" onClick={() => handleDone(todo.id)}>
